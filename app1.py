ed6e435486d608653e8525b5cc6cf94c054645d5
Agustin Esteves <agustinesteves2003@gmail.com>
2025-07-20 21:30:14 -0300

    Update app1.py

--- app1.py
+++ app1.py
@@ -5,16 +5,17 @@ st.set_page_config(page_title="Portafolio de Agustin Esteves", layout="wide")
 
 # --- Encabezado ---
 st.title("¡Hola! Soy Agustin Esteves")
-st.write("Bienvenido a mi portafolio de proyectos de Streamlit.")
+st.write("Bienvenido/a a mi portafolio de proyectos de Streamlit. Este espacio está dedicado a exhibir mis proyectos en Big Data y Machine Learning, donde aplico técnicas avanzadas para resolver problemas complejos, 
+identificando patrones significativos y generando insights accionables que impactan directamente en la toma de decisiones económicas.")
 
 st.markdown("---")
 
 # --- Acerca de mí (Opcional) ---
 st.header("Acerca de mí")
-st.write("Soy un estudiante de ultimo año de economia en la Facultad de Ciencias Economicas de la UBA con pasión por crear aplicaciones interactivas y útiles.")
-st.write("""
-    Actualmente me estoy adentrando en el mundo del Data Science:
-    aprendi a cargar, filtrar y limpiar grandes volumenes de datos con Pandas y a crear modelos de aprendizaje supervisado
+st.write("Soy un estudiante de ultimo año de economia en la Facultad de Ciencias Economicas de la UBA. Mi formación en economía me ha proporcionado una sólida base en análisis cuantitativo, teoría de juegos
+y modelado econométrico, lo que me permite abordar los problemas desde una perspectiva holística y entender el contexto económico detrás de cada conjunto de datos.")
+st.write("""Mi interés en la ciencia de datos surgió al darme cuenta del potencial ilimitado de estas herramientas para mejorar la precisión de las predicciones económicas, optimizar recursos y descubrir
+oportunidades que los métodos tradicionales no pueden revelar. Aprendi a cargar, filtrar y limpiar grandes volumenes de datos con Pandas y a crear modelos de aprendizaje supervisado
     como Random Forests y XGBoost. Tengo conocimientos intermedios en lenguaje Python y basicos en STATA y R.
 """)
 st.markdown("---")
@@ -27,7 +28,7 @@ with col1:
     
     # Proyecto 1
     st.subheader("1. Baja de clientes en empresa de telecomunicaciones")
-    st.write("El modelo es un XGBoost con un Recall del 82% que busca detectar aquellos clientes que dejaran el servicio para que la empresa haga un esfuerzo por retenerlos.")
+    st.write("Este modelo es un XGBoost con un Recall del 82% que busca detectar aquellos clientes que dejaran el servicio para que la empresa haga un esfuerzo por retenerlos.")
     st.image("telco.PNG") # Reemplaza con una captura de pantalla de tu app
     st.write("[Haga click aqui para ver el proyecto en accion](https://churn-telecomunicaciones-dyrhttuifv7e4phpo9rfqf.streamlit.app/)") # Enlace a la app desplegada
     st.write("[Código Fuente en GitHub](https://github.com/AgustinEsteves2003/churn-telecomunicaciones)")
@@ -37,7 +38,7 @@ with col2:
     # Proyecto 2
     st.subheader("2. Default crediticio en Taiwan")
     st.write("Creado a modo de trabajo final para la materia Big Data & Machine Learning de la facultad, este modelo de Random Forests con un Recall del 84% "
-             "que busca predecir la probabilidad de impago de la tarjeta de credito del cliente el proximo mes.")
+             " busca predecir la probabilidad de impago de la tarjeta de credito del cliente el proximo mes.")
     st.image("taiwan.PNG") # Reemplaza con una captura de pantalla de tu app
     st.write("[Haga click aqui para ver el proyecto en accion](https://taiwan-big-data-app-itapp2x9fdrmeen2w3itqft.streamlit.app/)")
     st.write("[Código Fuente en GitHub](https://github.com/AgustinEsteves2003/Taiwan-Big-Data-App)")
